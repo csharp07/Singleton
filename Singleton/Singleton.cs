d8f9883a137e1836366e24801e624aab26410f62
Harsh Gupta <Harshdeepgupta49@gmail.com>
2019-01-07 06:30:36 -0500

    Added singleton for multithread
    Co-Authored-By: Vineforce It Services Pvt. Ltd. <harshgupta71@users.noreply.github.com>

--- Singleton/Singleton.cs
+++ Singleton/Singleton.cs
@@ -9,31 +9,35 @@ namespace Singleton
     public sealed class Singleton
     {
         private static int counter = 0;
-        private static Singleton instance = null;
-
-        private static readonly object obj = new object();
-
-        public static Singleton GetInstance
-        {
-            get
-            {
-                if (instance == null)
-                {
-                    lock (obj)
-                    {
-                        if (instance == null)
-                            instance = new Singleton();
-                    }
-                }
-                return instance;
-            }
-        }
 
         private Singleton()
         {
             counter++;
             Console.WriteLine("Counter: " + counter.ToString());
         }
+
+
+        private static readonly Singleton instance = new Singleton();
+
+       // private static readonly object obj = new object();
+
+        public static Singleton GetInstance
+        {
+            get
+            {
+                //if (instance == null)
+                //{
+                //    lock (obj)
+                //    {
+                //        if (instance == null)
+                //            instance = new Singleton();
+                //    }
+                //}
+                return instance;
+            }
+        }
+
+     
 
         public void PrintDetails (string message)
         {
