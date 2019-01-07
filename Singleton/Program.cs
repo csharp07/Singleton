b214da851f3e1a76be779701e24ecea0829aa62a
Harsh Gupta <Harshdeepgupta49@gmail.com>
2019-01-07 05:59:52 -0500

    Harsh
    Singleton added
    
    Co-Authored-By: Vineforce It Services Pvt. Ltd. <harshgupta71@users.noreply.github.com>

---
+++ Singleton/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton First = Singleton.GetInstance;
+            First.PrintDetails("First Print");
+
+            Singleton Second = Singleton.GetInstance;
+            Second.PrintDetails("Second Print");
+
+            Console.ReadLine();
+        }
+    }
+}
